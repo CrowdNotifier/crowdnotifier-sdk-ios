93b94009277804ee347d5067a8f5709aa0fb4ccd
Matthias Felix <felix@ubique.ch>
2020-11-13 07:14:06 +0100

    Set deployment target to iOS 10, remove sample app.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CrowdNotifierSDK",
     platforms: [
-        .iOS("11.0"),
+        .iOS("10.0"),
     ],
     products: [
         .library(
